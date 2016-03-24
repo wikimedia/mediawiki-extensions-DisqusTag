4bb9406ba09ebab58cac376058d220e59a5897c8
Paladox <thomasmulhall410@yahoo.com>
2016-03-24 23:48:59 +0000

    Add npm entry point
    Change-Id: Ib3a91d9f5827def8f8fef58f2db18dde824c2697

--- DisqusTag.js
+++ DisqusTag.js
@@ -1,23 +1,23 @@
 window.showDisqusDialog = function( id ) {
 
-	$( '#disqus_dialog' ).dialog({ 'width': 800, 'position': 'top' });
+	jQuery( '#disqus_dialog' ).dialog( { 'width': 800, 'position': 'top' } );
 
 	var identifier = url = location.protocol + '//' + location.hostname + location.pathname + '#!' + id
 
 	//Reset Disqus to show the thread corresponding to the clicked button
-	DISQUS.reset({
+	DISQUS.reset( {
 		reload: true,
 		config: function () {
 			this.page.identifier = identifier;
 			this.page.url = url;
 		}
-	});
+	} );
 }
 
-$(function() {
+jQuery( function() {
 	var disqus = document.createElement( 'script' );
 	disqus.type = 'text/javascript';
 	disqus.async = true;
 	disqus.src = '//' + egDisqusShortname + '.disqus.com/embed.js';
-	document.getElementsByTagName( 'body' )[0].appendChild( disqus );
-});
+	document.getElementsByTagName( 'body' )[ 0 ].appendChild( disqus );
+} );
