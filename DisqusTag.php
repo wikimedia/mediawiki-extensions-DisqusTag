ee933331418e08e678cf95b914e312564a91ba85
LukBukkit <luk.bukkit@gmail.com>
2018-01-09 23:45:12 +0100

    Using the extension registration
    Bug: T184570
    Change-Id: I9e11803326292da407629f0c8167f59240cad4da

--- DisqusTag.php
+++ DisqusTag.php
@@ -1,26 +1,15 @@
 <?php
 
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'DisqusTag',
-	'descriptionmsg' => 'disqustag-desc',
-	'version' => '0.3.0',
-	'author' => 'Luis Felipe Schenone',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:DisqusTag',
-);
-
-$wgResourceModules['ext.DisqusTag'] = array(
-	'scripts' => 'DisqusTag.js',
-	'styles' => 'DisqusTag.css',
-	'position' => 'bottom',
-	'dependencies' => array( 'jquery.ui.dialog' ),
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'DisqusTag',
-);
-
-$wgMessagesDirs['DisqusTag'] = __DIR__ . '/i18n';
-$wgAutoloadClasses['DisqusTag'] = __DIR__ . '/DisqusTag.body.php';
-
-$wgHooks['BeforePageDisplay'][] = 'DisqusTag::addModule';
-$wgHooks['ParserFirstCallInit'][] = 'DisqusTag::setParserHook';
-$wgHooks['SkinAfterContent'][] = 'DisqusTag::addDisqusElements';
+if ( function_exists( 'DisqusTag' ) ) {
+	wfLoadExtension( 'DisqusTag' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['DisqusTag'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the DisqusTag extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the DisqusTag extension requires MediaWiki 1.25+' );
+}
