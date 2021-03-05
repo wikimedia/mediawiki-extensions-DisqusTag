4dd89491c8502519cd13bb4bf10c1a4a8f4cc384
zoranzoki21 <zorandori4444@gmail.com>
2021-03-05 02:23:09 +0100

    Add phpcs and make it pass
    Change-Id: I7992ac09ca48ab902d7f6d7ef6dae5ff16608b97

--- DisqusTag.php
+++ DisqusTag.php
@@ -2,16 +2,25 @@
 
 class DisqusTag {
 
+	/**
+	 * @param OutputPage &$out
+	 */
 	public static function addModule( &$out ) {
 		$out->addModules( 'ext.DisqusTag' );
-		return true;
 	}
 
+	/**
+	 * @param Parser $parser
+	 */
 	public static function setParserHook( Parser $parser ) {
 		$parser->setHook( 'disqus', 'DisqusTag::renderDisqusLink' );
-		return true;
 	}
 
+	/**
+	 * @param mixed &$data
+	 * @param Skin $skin
+	 * @return mixed
+	 */
 	public static function addDisqusElements( &$data, Skin $skin ) {
 		global $egDisqusShortname;
 		$data .= '<div id="disqus_dialog" title="Discuss"><div id="disqus_thread"></div></div>';
@@ -19,6 +28,13 @@ class DisqusTag {
 		return true;
 	}
 
+	/**
+	 * @param string $input
+	 * @param array $args
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return string[]
+	 */
 	public static function renderDisqusLink( $input, array $args, Parser $parser, PPFrame $frame ) {
 		$id = '';
 		if ( array_key_exists( 'id', $args ) ) {
